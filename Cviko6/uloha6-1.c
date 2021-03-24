a1a3bb38abc13929e11cb81df4eb293f198c6275
Mahrkeenerh <samuelbuban@gmail.com>
2021-03-24 12:32:14 +0100

    Uloha 6-1

---
+++ Cviko6/uloha6-1.c
@@ -0,0 +1,45 @@
+// uloha6-1.c -- Samuel Bubán, 23.3.2021 15:07
+
+#include <stdio.h>
+
+
+void combinations(int max_n, int current_n, int k, int out) {
+
+    if (current_n == max_n) {
+
+        int reverse = 0;
+
+        while (out > 0) {
+
+            reverse *= 10;
+            reverse += out % 10;
+            out /= 10;
+        }
+
+        while (reverse > 0) {
+
+            printf("%d ", reverse % 10);
+            reverse /= 10;
+        }
+
+        printf("\n");
+        return;
+    }
+
+    for (int i = 0; i < k; i++) {
+
+        combinations(max_n, current_n + 1, k, out * 10 + i + 1);
+    }
+}
+
+
+int main() {
+
+    int n, k;
+
+    scanf("%d %d", &n, &k);
+
+    combinations(n, 0, k, 0);
+
+    return 0;
+}
